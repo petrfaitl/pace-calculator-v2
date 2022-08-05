bc094b5eea545601c8a9c33caa7668a0377c3bbc
Petr Faitl <pfaitl@gmail.com>
2022-08-05 14:27:36 +1200

    Installs PWA

--- src/main.js
+++ src/main.js
@@ -5,5 +5,6 @@ import store from "./store";
 import "@/assets/tailwind.css";
 import "./assets/tailwind.css";
 import { state } from "@/store/store";
+import "./registerServiceWorker";
 
 createApp(App).use(store).use(router).provide("State", state).mount("#app");
--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,13 @@
 const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
   transpileDependencies: true,
+  pwa: {
+    name: "Pace Calculator",
+    short_name: "Pace Calc",
+    themeColor: "#06b6d4",
+    msTileColor: "#000000",
+    appleMobileWebAppCapable: true,
+    appleMobileWebAppStatusBarColor: "black",
+    display: "standalone",
+  },
 });
