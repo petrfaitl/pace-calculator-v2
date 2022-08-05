be527c0ebac095f2f8757d761b75397ee7ecb6de
Petr Faitl <pfaitl@gmail.com>
2022-08-05 13:09:41 +1200

    Adds button disabled states

--- src/views/CalculateView.vue
+++ src/views/CalculateView.vue
@@ -48,7 +48,7 @@
       <button
         type="submit"
         :disabled="!validInputs"
-        class="h-16 w-full font-medium uppercase rounded-md disabled:translate-y-0 disabled:shadow-none disabled:bg-cyan-500/30 disabled:cursor-not-allowed active:-translate-y-0.5 bg-cyan-500 text-white active:bg-cyan-600 active:shadow-lg dark:hover:text-cyan-50 dark:bg-cyan-500 dark:active:bg-cyan-500/70 transition select-none"
+        class="h-16 w-full font-medium uppercase rounded-md disabled:translate-y-0 disabled:shadow-none disabled:opacity-40 disabled:active:bg-cyan-500 disabled:dark:active:bg-cyan-500 disabled:dark:opacity-40 disabled:dark:active:shadow-none disabled:cursor-not-allowed active:-translate-y-0.5 bg-cyan-500 text-white active:bg-cyan-600 active:shadow-lg dark:hover:text-cyan-50 dark:bg-cyan-500 dark:active:bg-cyan-500/70 transition select-none"
       >
         View Results
       </button>
