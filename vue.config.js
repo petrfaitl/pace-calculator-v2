46f97dce628dd7a16aa14f8fae1e4b9e0a9dbc4b
Petr Faitl <pfaitl@gmail.com>
2022-08-20 10:45:32 +1200

    Rename to Pace Converter

--- vue.config.js
+++ vue.config.js
@@ -2,7 +2,7 @@ const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
   transpileDependencies: true,
   pwa: {
-    name: "Pace Calculator",
+    name: "Pace Converter",
     short_name: "Pace Calc",
     themeColor: "#06b6d4",
     msTileColor: "#000000",
