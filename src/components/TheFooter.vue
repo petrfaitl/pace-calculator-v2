1f4ec725c02c351f5367a82bc0b0adfd2a79a5b9
Petr Faitl <pfaitl@gmail.com>
2023-03-17 19:59:42 +1300

    Sets app version positioning

--- src/components/TheFooter.vue
+++ src/components/TheFooter.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="container px-4 text-left pt-8">
+  <div class="container px-4 text-left mt-48">
     <div class="text-xs text-cyan-900 dark:text-cyan-100">
       App version: {{ appVersion }}
     </div>
